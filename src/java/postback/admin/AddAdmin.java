f0d092f50145ccd22ec65c7eb137dbaada513438
sukhvir <sukhvir@sukhvir-PC>
2017-02-22 00:27:10 +0530

    added header page

--- src/java/postback/admin/AddAdmin.java
+++ src/java/postback/admin/AddAdmin.java
@@ -18,15 +18,15 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(urlPatterns = "/admin/admins/addadminpost")
 public class AddAdmin extends HttpServlet {
-
+    
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         
     }
-
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        resp.sendRedirect("/OAS/error");
     }
-
+    
 }
