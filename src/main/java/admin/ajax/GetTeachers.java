13d56abf28adabccf15d325c51cc9671f9798647
sthapar <1234Charmender>
2019-09-19 19:05:27 -0400

    refactoring the ajax admin package

--- src/main/java/admin/ajax/GetTeachers.java
+++ src/main/java/admin/ajax/GetTeachers.java
@@ -3,6 +3,7 @@ package admin.ajax;
 import com.google.gson.*;
 import entities.*;
 import org.apache.commons.lang3.StringUtils;
+import org.gradle.internal.impldep.com.google.api.client.json.Json;
 import org.hibernate.Session;
 import utility.AjaxController;
 import utility.CriteriaHolder;
@@ -33,26 +34,96 @@ public class GetTeachers extends AjaxController {
 
         var holder = CriteriaHolder.getQueryHolder(session, Teacher.class);
 
-        var userJoin = holder.getRoot()
-                .join(Teacher_.user, JoinType.INNER);
-
         List<Predicate> predicates = new ArrayList<>();
 
-        if (StringUtils.isNotBlank(searchText)) {
-            addSearchConditions(searchText.trim(), holder, userJoin, predicates);
+
+        addSearchConditions(searchText.trim(), holder, predicates);
+        addPageValueCondition(pageValue, holder, predicates);
+        processAdditionalData(holder, predicates, additionalData);
+
+        var teachers = getTeachers(session, holder, predicates);
+
+        var output = super.getSuccessJson();
+
+        addDataToJson(output, teachers);
+
+        out.println(new Gson().toJson(output));
+    }
+
+    private Join<Teacher, User> getUserJoin(CriteriaHolder<CriteriaQuery<Teacher>, Teacher> holder) {
+        return holder.getJoin(Teacher_.user)
+                .orElseGet(() -> joinUser(holder));
+    }
+
+    private Join<Teacher, User> joinUser(CriteriaHolder<CriteriaQuery<Teacher>, Teacher> holder) {
+        return holder.getRoot()
+                .join(Teacher_.user, JoinType.INNER);
+    }
+
+
+    private void addPageValueCondition(String pageValue, CriteriaHolder<CriteriaQuery<Teacher>, Teacher> holder, List<Predicate> predicates) {
+        if (pageValue.isBlank()) {
+            return;
         }
 
-        if (StringUtils.isNotBlank(pageValue)) {
-            addPageValueCondition(pageValue, holder, predicates);
-        }
+        String[] names = pageValue.split("--");
+        predicates.add(
+                holder.getBuilder()
+                        .greaterThan(holder.getRoot().get(Teacher_.fName), names[0])
+        );
+        predicates.add(
+                holder.getBuilder()
+                        .greaterThan(holder.getRoot().get(Teacher_.lName), names[1])
+        );
+    }
 
-        if (StringUtils.isNotBlank(additionalData)) {
-            var additionalDataJson = new JsonParser()
+
+    private void processAdditionalData(CriteriaHolder<CriteriaQuery<Teacher>, Teacher> holder, List<Predicate> predicates, String additionalData) {
+
+        JsonObject additionalDataJson = parseAdditionData(additionalData);
+
+        // if the additional data contains the departmentId then get the teacher of the department.
+        Optional.ofNullable(additionalDataJson.get("departmentId"))
+                .map(JsonElement::getAsLong)
+                .ifPresent(departmentId -> addDepartmentCondition(holder, predicates, additionalDataJson, departmentId));
+
+    }
+
+    private JsonObject parseAdditionData(String additionalData) {
+        try {
+            return new JsonParser()
                     .parse(additionalData)
                     .getAsJsonObject();
-
-            processAdditionalData(holder, predicates, additionalDataJson);
+        } catch (Exception e) {
+            return new JsonObject();
         }
+    }
+
+
+    private void addDepartmentCondition(CriteriaHolder<CriteriaQuery<Teacher>, Teacher> holder, List<Predicate> predicates, JsonObject additionalDataJson, Long departmentId) {
+
+        var subQuery = holder.getQuery()
+                .subquery(UUID.class);
+        var subQueryRoot = subQuery.from(TeacherDepartmentLink.class);
+
+        subQuery.where(
+                holder.getBuilder().equal(subQueryRoot.get(TeacherDepartmentLink_.department), departmentId)
+        );
+
+        var innerJoin = subQueryRoot.join(TeacherDepartmentLink_.teacher, JoinType.INNER);
+        subQuery.select(innerJoin.get(Teacher_.id));
+
+        predicates.add(
+                holder.getRoot().get(Teacher_.id).in(subQuery)
+        );
+
+
+    }
+
+    private List<Teacher> getTeachers(Session session, CriteriaHolder<CriteriaQuery<Teacher>, Teacher> holder, List<Predicate> predicates) {
+
+        var graph = session.createEntityGraph(Teacher.class);
+        graph.addAttributeNode(Teacher_.user);
 
         holder.getQuery()
                 .where(
@@ -68,73 +139,47 @@ public class GetTeachers extends AjaxController {
                                 .asc(holder.getRoot().get(Teacher_.lName))
                 );
 
-        var graph = session.createEntityGraph(Teacher.class);
-        graph.addAttributeNode(Teacher_.user);
-
-        var results = session.createQuery(holder.getQuery())
+        return session.createQuery(holder.getQuery())
                 .setMaxResults(super.getPageSize() + 1)
                 .setReadOnly(true)
                 .applyLoadGraph(graph)
                 .getResultList();
+    }
 
-        var output = super.getSuccessJson();
 
-        if (results.size() == super.getPageSize() + 1) {
-            output.addProperty("more", true);
-            results.remove(results.size() - 1);
-        } else {
-            output.addProperty("more", false);
-        }
+    private void addDataToJson(JsonObject successJson, List<Teacher> teachers) {
 
-        JsonArray data = results.stream()
+        setMorePageProperty(successJson, teachers);
+        removeExtraTeacher(teachers);
+        setPageValueProperty(successJson, teachers);
+
+        JsonArray data = teachers.stream()
                 .map(this::mapTeacherToJson)
                 .collect(JsonArray::new, JsonArray::add, JsonArray::addAll);
 
-        output.add(DATA, data);
-        if (data.size() > 0) {
-            var teacher = results.get(results.size() - 1);
-            output.addProperty("pageValue", teacher.getFName() + "--" + teacher.getLName());
-        } else {
-            output.addProperty("pageValue", "");
+        successJson.add(DATA, data);
+
+    }
+
+    private void setMorePageProperty(JsonObject successJson, List<Teacher> teachers) {
+        successJson.addProperty("more", teachers.size() == super.getPageSize() + 1);
+    }
+
+    private void removeExtraTeacher(List<Teacher> teachers) {
+        if (teachers.size() == super.getPageSize() + 1) {
+            teachers.remove(teachers.size() - 1);
         }
-
-        out.println(new Gson().toJson(output));
     }
 
-    private void addPageValueCondition(String pageValue, CriteriaHolder<CriteriaQuery<Teacher>, Teacher> holder, List<Predicate> predicates) {
-        var names = pageValue.split("--");
-        predicates.add(
-                holder.getBuilder()
-                        .greaterThan(holder.getRoot().get(Teacher_.fName), names[0])
-        );
-        predicates.add(
-                holder.getBuilder()
-                        .greaterThan(holder.getRoot().get(Teacher_.lName), names[1])
-        );
+    private void setPageValueProperty(JsonObject successJson, List<Teacher> teachers) {
+        if (teachers.size() > 0) {
+            var teacher = teachers.get(teachers.size() - 1);
+            successJson.addProperty("pageValue", teacher.getFName() + "--" + teacher.getLName());
+        } else {
+            successJson.addProperty("pageValue", "");
+        }
     }
 
-    private void processAdditionalData(CriteriaHolder<CriteriaQuery<Teacher>, Teacher> holder, List<Predicate> predicates, JsonObject additionalDataJson) {
-        // if the additional data contains the departmentId then get the teacher of the department.
-        Optional.ofNullable(additionalDataJson.get("departmentId"))
-                .map(JsonElement::getAsLong)
-                .ifPresent(departmentId -> addDepartmentCondition(holder, predicates, additionalDataJson, departmentId));
-
-    }
-
-    private void addDepartmentCondition(CriteriaHolder<CriteriaQuery<Teacher>, Teacher> holder, List<Predicate> predicates, JsonObject additionalDataJson, Long departmentId) {
-        var subQuery = holder.getQuery()
-                .subquery(UUID.class);
-        var subQueryRoot = subQuery.from(TeacherDepartmentLink.class);
-        subQuery.where(
-                holder.getBuilder().equal(subQueryRoot.get(TeacherDepartmentLink_.department), departmentId)
-        );
-        var innerJoin = subQueryRoot.join(TeacherDepartmentLink_.teacher, JoinType.INNER);
-        subQuery.select(innerJoin.get(Teacher_.id));
-
-        predicates.add(
-                holder.getRoot().get(Teacher_.id).in(subQuery)
-        );
-    }
 
     private JsonObject mapTeacherToJson(Teacher teacher) {
         var jsonObject = new JsonObject();
@@ -149,33 +194,46 @@ public class GetTeachers extends AjaxController {
 
     }
 
-    private void addSearchConditions(String searchText, CriteriaHolder<CriteriaQuery<Teacher>, Teacher> holder, Join<Teacher, User> userJoin, List<Predicate> predicates) {
+    private void addSearchConditions(String searchText, CriteriaHolder<CriteriaQuery<Teacher>, Teacher> holder, List<Predicate> predicates) {
+
+        if (searchText.isBlank()) {
+            return;
+        }
+
         List<Predicate> orPredicates = new ArrayList<>();
+
+        Join<Teacher, User> userJoin = getUserJoin(holder);
 
         orPredicates.add(
                 holder.getBuilder()
                         .like(holder.getBuilder().lower(userJoin.get(User_.email)), searchText.toLowerCase() + "%")
         );
+
         orPredicates.add(
                 holder.getBuilder()
                         .like(holder.getBuilder().lower(holder.getRoot().get(Teacher_.fName)), searchText.toLowerCase() + "%")
         );
+
         orPredicates.add(
                 holder.getBuilder()
                         .like(holder.getBuilder().lower(holder.getRoot().get(Teacher_.lName)), searchText.toLowerCase() + "%")
         );
 
+        //adds user status search condition
         for (UserStatus userStatus : UserStatus.values()) {
             if (userStatus.toString()
                     .toLowerCase()
                     .contains(searchText)) {
+
                 orPredicates.add(
                         holder.getBuilder()
                                 .equal(userJoin.get(User_.status), userStatus)
                 );
+
             }
         }
 
+        //adds search by number condition
         if (StringUtils.isNumeric(searchText)) {
             var number = Long.parseLong(searchText);
             number = number * (long) Math.pow(10, (10 - searchText.trim().length()));
